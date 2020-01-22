47e095fa33992396b3b21b18f3bcec910f25e3a2
flynn <crf204@gmail.com>
2020-01-21 22:07:26 -0500

    add db seeds

---
+++ migrations/seeds.py
@@ -0,0 +1,86 @@
+import datetime
+import logging
+import random
+import subprocess
+
+from pypistats.application import create_app
+from pypistats.application import db
+from pypistats.models.download import OverallDownloadCount
+from pypistats.models.download import PythonMajorDownloadCount
+from pypistats.models.download import PythonMinorDownloadCount
+from pypistats.models.download import RecentDownloadCount
+from pypistats.models.download import SystemDownloadCount
+
+# required to use the db models outside of the context of the app
+app = create_app()
+app.app_context().push()
+
+# use the currently installed dependencies as seed packages
+result = subprocess.run(["poetry", "show"], stdout=subprocess.PIPE)
+output = result.stdout.decode()
+
+# extract just the package names from the output
+packages = []
+for line in output.split("\n")[:-1]:
+    packages.append(line.split(" ")[0])
+
+logging.info(packages)
+
+# take the last 120 days
+end_date = datetime.date.today()
+date_list = [end_date - datetime.timedelta(days=x) for x in range(120)][::-1]
+
+baseline = 1000
+
+# build a bunch of seed records with random values
+records = []
+for package in packages + ["__all__"]:
+    print("Seeding: " + package)
+
+    for idx, category in enumerate(["day", "week", "month"]):
+        record = RecentDownloadCount(
+            package=package, category=category, downloads=baseline * (idx + 1) + random.randint(-100, 100)
+        )
+        records.append(record)
+
+    for date in date_list:
+
+        for idx, category in enumerate(["with_mirrors", "without_mirrors"]):
+            record = OverallDownloadCount(
+                date=date,
+                package=package,
+                category=category,
+                downloads=baseline * (idx + 1) + random.randint(-100, 100),
+            )
+            records.append(record)
+
+        for idx, category in enumerate(["2", "3"]):
+            record = PythonMajorDownloadCount(
+                date=date,
+                package=package,
+                category=category,
+                downloads=baseline * (idx + 1) + random.randint(-100, 100),
+            )
+            records.append(record)
+
+        for idx, category in enumerate(["2.7", "3.4", "3.5", "3.6", "3.7", "3.8"]):
+            record = PythonMinorDownloadCount(
+                date=date,
+                package=package,
+                category=category,
+                downloads=baseline * (idx + 1) + random.randint(-100, 100),
+            )
+            records.append(record)
+
+        for idx, category in enumerate(["windows", "linux", "darwin"]):
+            record = SystemDownloadCount(
+                date=date,
+                package=package,
+                category=category,
+                downloads=baseline * (idx + 1) + random.randint(-100, 100),
+            )
+            records.append(record)
+
+# push to the local database
+db.session.bulk_save_objects(records)
+db.session.commit()
