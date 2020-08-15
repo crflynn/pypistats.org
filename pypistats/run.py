badca6339f33a2a99438c5d9d6e225eb7c82a785
flynn <crf204@gmail.com>
2020-08-15 18:31:41 -0400

    deploy

--- pypistats/run.py
+++ pypistats/run.py
@@ -2,6 +2,8 @@
 import os
 
 from flask import g
+from flask import redirect
+from flask import request
 from flask import session
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
@@ -26,6 +28,12 @@ app.logger.info(f"Environment: {env}")
 @app.before_request
 def before_request():
     """Execute before requests."""
+    # http -> https
+    scheme = request.headers.get("X-Forwarded-Proto")
+    if scheme and scheme == "http" and request.url.startswith("http://"):
+        url = request.url.replace("http://", "https://", 1)
+        return redirect(url, code=301)
+    # set user
     g.user = None
     if "user_id" in session:
         g.user = User.query.get(session["user_id"])
